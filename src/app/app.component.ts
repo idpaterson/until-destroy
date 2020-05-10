293d3db0113b40164eabe3af9c129542fc954cbf
idpaterson <idpaterson@users.noreply.github.com>
2020-05-10 09:18:30 -0400

    Use the @UntilDestroy decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
